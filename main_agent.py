4ff441730d9b00cdfc30ae39dddf995c3982f474
Daniel <danielzawi04@gmail.com>
2026-02-26 09:07:40 +0100

    Refactor: Optimize orchestrator, enforce types, and clean up daemon process

--- main_agent.py
+++ main_agent.py
@@ -1,12 +1,16 @@
+import os
 import time
 from system_monitor import check_hardware
 from healthcheck import run_healthcheck
 
-list_url = ["https://google.com", "https://api.github.com", "https://httpbin.org/status/404", "https://httpbin.org/status/500","https://adres-ktory-na-pewno-nie-istnieje-12345.com"]
+URLS_TO_CHECK = os.environ.get("URLS_TO_CHECK", "https://google.com,https://api.github.com,https://httpbin.org/status/404,https://httpbin.org/status/500,https://adres-ktory-na-pewno-nie-istnieje-12345.com").split(",")
+CHECK_INTERVAL = int(os.environ.get("CHECK_INTERVAL", "60"))
 
-while True:
-    print("=== URUCHAMIAM CYKL SPRAWDZAJĄCY ===")
-    check_hardware()
-    run_healthcheck(list_url)
-    print("=== OCZEKIWANIE 10S ====")
-    time.sleep(10)
+def run_agent():
+    while True:
+        check_hardware()
+        run_healthcheck(URLS_TO_CHECK)
+        time.sleep(CHECK_INTERVAL)
+
+if __name__ == "__main__":
+    run_agent()
